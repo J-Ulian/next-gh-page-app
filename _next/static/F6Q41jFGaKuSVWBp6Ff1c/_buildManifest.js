191a2b903e8354aa3f764458ec0a30238f14b293
J Ulian <yhudatov@aori.ru>
2022-02-12 23:51:46 +0300

    Deploy Next.js to gh-pages

---
+++ _next/static/F6Q41jFGaKuSVWBp6Ff1c/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-a64d7aa6fc74e8f8.js"],"/_error":["static/chunks/pages/_error-2f883067a14f4c4a.js"],"/about":["static/chunks/pages/about-5cd9ec983dcffe13.js"],sortedPages:["/","/_app","/_error","/about"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
